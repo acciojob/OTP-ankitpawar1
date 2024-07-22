730e4ee5d6371666579b3409cf77dace16fd5499
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-22 20:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('.code');
+    inputs[0].focus();
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('keydown', (e) => {
+            if (e.key >= 0 && e.key <= 9) {
+                inputs[index].value = '';
+                setTimeout(() => inputs[index + 1]?.focus(), 10);
+            } else if (e.key === 'Backspace') {
+                setTimeout(() => inputs[index - 1]?.focus(), 10);
+            }
+        });
+    });
+});
